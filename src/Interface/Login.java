38fa648e61aa99d1db1fde379486bd7003361788
Jefferson Geovanny Moreno Perez <34329727+Jefferson201603047@users.noreply.github.com>
2019-10-30 03:16:28 -0600

    Login and Register

---
+++ src/Interface/Login.java
@@ -0,0 +1,148 @@
+package Interface;
+
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JeffGeo
+ */
+public class Login extends javax.swing.JFrame {
+
+    public Login() {
+        initComponents();
+        Load_Images(Login);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        Password = new javax.swing.JPasswordField();
+        Username = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        New_User = new javax.swing.JLabel();
+        Forgot_Pass = new javax.swing.JLabel();
+        Login = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Login");
+
+        jLabel1.setText("Username ");
+
+        jLabel2.setText("Password");
+
+        jButton1.setText("SIGN IN");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        New_User.setForeground(new java.awt.Color(51, 51, 255));
+        New_User.setText("Create a new account");
+        New_User.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                New_UserMouseClicked(evt);
+            }
+        });
+
+        Forgot_Pass.setForeground(new java.awt.Color(51, 51, 255));
+        Forgot_Pass.setText("Forgot your password?");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(New_User, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 61, Short.MAX_VALUE)
+                        .addComponent(Forgot_Pass, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(81, 81, 81)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)
+                            .addComponent(Username)
+                            .addComponent(Password)
+                            .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(Login, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(116, 116, 116))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(Login, javax.swing.GroupLayout.DEFAULT_SIZE, 98, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Username, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Password, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1)
+                .addGap(30, 30, 30)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(New_User)
+                    .addComponent(Forgot_Pass))
+                .addGap(27, 27, 27))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String User = Username.getText();
+        String Pass = Password.getText();
+        
+        if(User.equalsIgnoreCase("Admin") && Pass.equalsIgnoreCase("Admin")){
+            JOptionPane.showMessageDialog(null, "Welcome Administrator", "Information", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void New_UserMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_New_UserMouseClicked
+      int Resp = JOptionPane.showConfirmDialog(null, "Do you Want to Create a new User","Information",JOptionPane.YES_NO_OPTION);
+      
+      if(Resp == JOptionPane.YES_OPTION){
+          Register r = new Register();
+          r.setVisible(true);
+      }
+      if(Resp == JOptionPane.NO_OPTION){
+          System.out.println("Sin Abrir Form");
+      }
+    }//GEN-LAST:event_New_UserMouseClicked
+
+    private void Load_Images(JLabel login){
+        Image imglogin = new ImageIcon(getClass().getResource("../Images/login.png")).getImage();
+        Image newimglogin = imglogin.getScaledInstance(login.getWidth(), login.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(newimglogin);
+        login.setIcon(icon);
+    }
+ 
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Forgot_Pass;
+    private javax.swing.JLabel Login;
+    private javax.swing.JLabel New_User;
+    private javax.swing.JPasswordField Password;
+    private javax.swing.JTextField Username;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/Interface/Register.java
@@ -0,0 +1,244 @@
+package Interface;
+
+import java.awt.Color;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ *
+ * @author JeffGeo
+ */
+public class Register extends javax.swing.JFrame {
+
+  
+    public Register() {
+        initComponents();
+        Load_Images(register);
+        Listeners();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        pass = new javax.swing.JPasswordField();
+        cpass = new javax.swing.JPasswordField();
+        username = new javax.swing.JTextField();
+        UserV = new javax.swing.JLabel();
+        PassV = new javax.swing.JLabel();
+        Pass2V = new javax.swing.JLabel();
+        check = new javax.swing.JButton();
+        register = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Check In");
+
+        jLabel1.setText("UserName");
+
+        jLabel2.setText("Password");
+
+        jLabel3.setText("Confirm Password");
+
+        check.setText("CHECK IN");
+        check.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                checkActionPerformed(evt);
+            }
+        });
+
+        register.setText("jLabel4");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(cpass, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(Pass2V, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel3)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(pass, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(PassV, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(UserV, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(49, 49, 49)
+                        .addComponent(register, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(check, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(34, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(114, 114, 114)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(UserV, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(username))
+                        .addGap(23, 23, 23)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(pass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(PassV, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(23, 23, 23)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(cpass, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(Pass2V, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(check))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(register, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(20, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void checkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkActionPerformed
+       JOptionPane.showMessageDialog(null, "User Add to EDD Drive", "Information", JOptionPane.INFORMATION_MESSAGE);
+       dispose();
+    }//GEN-LAST:event_checkActionPerformed
+
+        
+    private void Load_Images(JLabel checkin){
+        Image imglogin = new ImageIcon(getClass().getResource("../Images/checkin.png")).getImage();
+        Image newimglogin = imglogin.getScaledInstance(checkin.getWidth(), checkin.getHeight(), Image.SCALE_SMOOTH);
+        ImageIcon icon = new ImageIcon(newimglogin);
+        checkin.setIcon(icon);
+    }
+    
+    private void Listeners(){
+        pass.getDocument().addDocumentListener(new DocumentListener() {
+           @Override
+           public void insertUpdate(DocumentEvent e) {
+               Pass();
+           }
+
+           @Override
+           public void removeUpdate(DocumentEvent e) {
+               Pass();
+           }
+
+           @Override
+           public void changedUpdate(DocumentEvent e) {
+               Pass();
+           }
+        });
+       
+        cpass.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                CPass();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                CPass();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                CPass();
+            }
+        });
+        
+        
+        username.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                Username();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                Username();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                Username();
+            }
+        });
+    }
+    
+    private void Pass(){
+        if(pass.getText().isEmpty()){
+            PassV.setText("Campo Obligatorio");
+            PassV.setForeground(Color.RED);
+            check.setEnabled(false);
+        }else{
+            if(pass.getText().length()<8){
+                PassV.setText("Invalid Password");
+                PassV.setForeground(Color.RED);
+                check.setEnabled(false);
+            }else{
+                PassV.setText("Valid Password");
+                PassV.setForeground(Color.BLUE);
+                check.setEnabled(true);
+            }
+        }
+    }
+    
+    private void CPass(){
+        if(cpass.getText().equals(pass.getText())){
+            Pass2V.setText("Password Match");
+            Pass2V.setForeground(Color.BLUE);
+            check.setEnabled(true);
+        }else{
+            Pass2V.setText("Passwords Do Not Match");
+            Pass2V.setForeground(Color.RED);
+            check.setEnabled(false);
+        }
+    }
+    
+    private void Username(){
+        if(username.getText().isEmpty()){
+           UserV.setText("Obligatory Field");
+           UserV.setForeground(Color.RED);
+           check.setEnabled(false);
+        }else{
+            UserV.setText("User Available");
+            UserV.setForeground(Color.BLUE);
+            check.setEnabled(true);
+        }
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Pass2V;
+    private javax.swing.JLabel PassV;
+    private javax.swing.JLabel UserV;
+    private javax.swing.JButton check;
+    private javax.swing.JPasswordField cpass;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPasswordField pass;
+    private javax.swing.JLabel register;
+    private javax.swing.JTextField username;
+    // End of variables declaration//GEN-END:variables
+}
